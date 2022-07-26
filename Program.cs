40fa65f75933db0b3665072a31c62395f8b2b943
Afamsv <afamsv1234@gmail.com>
2022-07-26 23:45:06 +0100

    I removed the class check and the Odd mehtod inorder to have one method running the code.

--- Program.cs
+++ Program.cs
@@ -8,52 +8,27 @@ namespace Even_and_odd_number_check
 {
     internal class Program
     {
-       class check
+        static void Evennumber(int number)
         {
-            public void Even(int r)
+            if (number % 2 == 0)
             {
-                if (r % 2 == 0)
-                {
-                    Console.WriteLine($"The {r} entered is even number");
-                }
-                else
-                {
-                    Console.WriteLine("This is not an even number");
-                }
+               Console.WriteLine($"The {number} entered is even number");
             }
-            public void odd(int h)
+            else
             {
-                if (h % 2 != 0)
-                {
-                    Console.WriteLine($"The {h} entered is odd number");
-                }
-                else
-                {
-                    Console.WriteLine("This is not an odd number");
-                }
+               Console.WriteLine($"The {number} entered is odd number");
             }
+            return;
         }
+          
         static void Main(string[] args)
         {
-            Console.WriteLine("Please select option: ");
-            Console.WriteLine("1. even ");
-            Console.WriteLine("2. odd ");
-            int source = Convert.ToInt32(Console.ReadLine());
             
-            check input = new check();
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("");
-            if (source == 1)
-            {
-                Console.WriteLine("Enter number");
-                int r = Convert.ToInt32(Console.ReadLine());
-                input.Even(r);
-            }
-            else if (source == 2)
-            {
-                Console.WriteLine("Enter number");
-                int h = Convert.ToInt32(Console.ReadLine());
-                input.odd(h);
-            }
+            Evennumber(number);
+            Console.WriteLine("");
         }
     }
 }
